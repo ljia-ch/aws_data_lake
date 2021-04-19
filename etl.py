da9d890f4ac3eeeca9afc1ab712e4b1a8876dd2b
ljia-ch <linjia.iphy@gmail.com>
2021-04-19 09:27:44 -0500

    first commit from work pc

--- etl.py
+++ etl.py
@@ -93,14 +93,16 @@ def process_log_data(spark, input_data, output_data):
 
     # create timestamp column from original timestamp column
     get_timestamp = udf(lambda x: str(int(int(x) / 1000)))
-    df_actions = df_actions.withColumn('datetime', get_datetime(df_actions.ts)) 
+    df_actions = df_actions.withColumn('timestamp', get_datetime(df_actions.ts)) 
     
     # create datetime column from original timestamp column
-    get_datetime = udf()
-    df = 
+    get_datetime = udf(lambda x: str(datetime.fromtimestamp(int(x)/1000)))
+    df_actions = df_actions.withColumn('datetime', get_datetime(df_actions.ts)) 
     
     # extract columns to create time table
-    time_table = 
+    time_table = df_actions.select('datetime')\
+                           .withColumn('start_time', df_actions.datetime)
+                           .withColumn('hour', hour('datetime'))
     
     # write time table to parquet files partitioned by year and month
     time_table
